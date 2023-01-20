76c7031960d311a49956c281646276fe957c905f
AndreyShukatka <9080106@gmail.com>
2023-01-20 17:06:55 +0300

    Замените поля CharField / TextField

--- addressapp/models.py
+++ addressapp/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class AddressCoordinate(models.Model):
-    address = models.TextField(unique=True, verbose_name='Адрес')
+    address = models.CharField(unique=True, max_length=200, verbose_name='Адрес')
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
 
