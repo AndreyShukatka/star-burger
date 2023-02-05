7b0122f8d0c9d247b18e790a867d97724927af5d
AndreyShukatka <9080106@gmail.com>
2023-02-05 18:50:54 +0300

    Поправил деплой

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,19 +1,27 @@
-#!/bin/bash
+# #!/bin/bash
+set -Eeuo pipefail
 git pull
-echo 'Git код обновлен'
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+if ! [ -e venv ]
+then
+python3 -m venv venv
+fi
+source venv/bin/activate
 pip install -r requirements.txt
-echo 'Библиотеки для Python установлены'
-npm ci --dev
-echo 'Пакет JS собран'
-./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./"
-echo 'запустили сборку фронтенда'
-python3 manage.py collectstatic
-echo 'Статика собрана'
-python3 manage.py makemigrations
-python3 manage.py migrate
-echo 'Миграция выполнена'
-systemctl stop nginx
-systemctl stop starburger
-systemctl start nginx
-systemctl start starburger
-echo "Деплой завершен"
+pip install gunicorn
+if [ -e static ]
+then
+sudo rm -rf static
+fi
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+sudo systemctl daemon-reload
+REVISION=$(git log -1 --pretty=format:'%H')
+COMMIT_AUTHOR=$(git log -1 --pretty=format:'%an')
+COMMIT_COMMENT=$(git log -1 --pretty=format:'%s')
+curl \
+-H "X-Rollbar-Access-Token: $ROLLBAR_SERVER_TOKEN" \
+-H "Content-Type: application/json" \
+-X POST 'https://api.rollbar.com/api/1/deploy' \
+-d '{"environment": "home_pk", "revision": "'"$REVISION"'", "rollbar_name": "john", "local_username": "'"$COMMIT_AUTHOR"'", "comment": "'"$COMMIT_COMMENT"'", "status": "succeeded"}'
+echo "Deploy completed successfully!"
