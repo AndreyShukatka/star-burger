4a51988fcec7028fe93130ade0180c908eb9d94d
AndreyShukatka <9080106@gmail.com>
2023-02-05 16:04:33 +0300

    Update settings.py

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -14,17 +14,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list(
-    'ALLOWED_HOSTS',
-    [
-        '127.0.0.1',
-        'localhost',
-        '81.163.30.207',
-        'russiansword.ru',
-        'http://www.russiansword.ru',
-        'www.russiansword.ru'
-    ]
-)
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost', '81.163.30.207', 'russiansword.ru', 'www.russiansword.ru'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
